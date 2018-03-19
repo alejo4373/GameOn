6c1b96584f05ea6b7c6e0735f9ab2b72d7617f17
KelvinRod9331 <kelvinrodriguez@ac.c4q.nyc>
2018-03-19 11:00:26 -0400

    Fixing

---
+++ frontend/src/Components/Home/Login.js
@@ -0,0 +1,136 @@
+import React from "react";
+import axios from "axios";
+import { Redirect } from "react-router";
+import { Link } from 'react-router-dom';
+
+
+class Login extends React.Component {
+
+  state = {
+    usernameInput: this.props.usernameInput || '',
+    passwordInput: this.props.passwordInput || '',
+    loggedIn: false,
+    message: '',
+    user: null
+  };
+
+  /**
+   * @func handleUserNameChange
+   Handles the users value and Set the State to that value
+   ~Kelvin
+   */
+
+  handleUsernameChange = e => {
+    this.setState({
+      usernameInput: e.target.value
+    });
+  };
+  /**
+   * @func handlePasswordChange
+   Handles the users value/password and Set the State to that value
+   ~Kelvin
+   */
+
+  handlePasswordChange = e => {
+    this.setState({
+      passwordInput: e.target.value
+    });
+  };
+  /**
+   * @func getUser
+   Retrieve the user who has logged In and set the state to that user
+   This will be used to determine if the user has logged in to redirect them to their profile when the user
+   hasn't logged out
+   ~Kelvin
+   */
+  getUser = () => {
+    axios.get('')
+    .then(res => {
+        this.setState({
+            user: res.data.data[0]
+        })
+    })
+  }
+  /**
+   * @func submitForm
+   Submit/Post The Input to Database to retrieve User
+   ~Kelvin
+   */
+
+  submitForm = e => {
+    e.preventDefault();
+    const { usernameInput, passwordInput } = this.state;
+
+    axios
+      .post('', {
+        username: usernameInput,
+        password: passwordInput
+      })
+      .then(res => {
+        this.setState({
+          loggedIn: true
+        });
+      })
+      .catch(err => {
+        this.setState({
+          usernameInput: '',
+          passwordInput: '',
+          message: 'username/password not found'
+        });
+      });
+
+  };
+  componentDidMount() {
+    this.getUser()
+  }
+  render() {
+    const { usernameInput, passwordInput, message, loggedIn, user } = this.state;
+    const { submitForm } = this;
+    
+    if (user) {
+      return <Redirect to='/user' />;
+    }
+    if (loggedIn) {
+      return <Redirect to="/user" />;
+    }
+
+    return (
+        <div id='parent'>
+            <div class='login-container' >
+                <div class='login-box'>
+                <h1 id='title'>GameOn</h1>
+                    <form onSubmit={this.submitForm}>
+                    <label>
+                        <input
+                        type="text"
+                        name="username"
+                        placeholder='Username or Email'
+                        value={usernameInput}
+                        onChange={this.handleUsernameChange}
+                        />
+                    </label>
+
+                    <label>
+                        <input
+                        type="password"
+                        name="username"
+                        placeholder='Password'
+                        value={passwordInput}
+                        onChange={this.handlePasswordChange}
+                        />
+                    </label>
+
+                    <input type="submit" value="Log In" />
+                    </form>
+                    <span>{message}</span>
+                </div>
+                <div class='login-box'>
+                Don't have an account? <Link to="/registration">Sign Up</Link>
+                </div>
+            </div>
+        </div>
+    );
+  }
+}
+
+export default Login;
---
+++ frontend/src/Components/Register/Form.js
@@ -0,0 +1,233 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { Redirect } from "react-router";
+
+class Registration extends Component {
+  state = {
+    emailInput: "",
+    fullNameInput: "",
+    usernameInput: "",
+    passwordInput: "",
+    confirmInput: "",
+    zipcodeInput: "",
+    message: "",
+    registered: false
+  };
+
+  handleUsernameChange = e => {
+    this.setState({
+      usernameInput: e.target.value
+    });
+  };
+
+  handlePasswordChange = e => {
+    this.setState({
+      passwordInput: e.target.value
+    });
+  };
+
+  handleConfirmChange = e => {
+    this.setState({
+      confirmInput: e.target.value
+    });
+  };
+
+  handleEmailChange = e => {
+    this.setState({
+      emailInput: e.target.value
+    });
+  };
+
+  handleFullNameChange = e => {
+    this.setState({
+      fullNameInput: e.target.value
+    });
+  };
+
+  handleZipCodeChange = e => {
+    if (typeof e.target.value === "number") {
+      this.setState({
+        zipcodeInput: e.target.value
+      });
+    }
+  };
+
+  submitForm = e => {
+    e.preventDefault();
+    const {
+      usernameInput,
+      passwordInput,
+      confirmInput,
+      emailInput,
+      fullNameInput,
+      registered,
+      zipcodeInput
+    } = this.state;
+    if (
+      !usernameInput ||
+      !passwordInput ||
+      !confirmInput ||
+      !emailInput ||
+      !fullNameInput ||
+      !zipcodeInput
+    ) {
+      this.setState({
+        passwordInput: "",
+        confirmInput: "",
+        message: "Please complete all required fields"
+      });
+      return;
+    }
+    if (passwordInput.length <= 7) {
+      this.setState({
+        passwordInput: "",
+        confirmInput: "",
+        message: "Password length must be at least 8 characters"
+      });
+      return;
+    }
+    if (passwordInput !== confirmInput) {
+      this.setState({
+        passwordInput: "",
+        confirmInput: "",
+        message: "Passwords do not match!"
+      });
+      return;
+    }
+    axios
+      .post("", {
+        username: usernameInput,
+        password: passwordInput,
+        email: emailInput,
+        full_name: fullNameInput,
+        zipcode: zipcodeInput
+      })
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          registered: true,
+          message: `Welcome to the site ${this.state.usernameInput}`
+        });
+      })
+      .catch(err => {
+        console.log("error: ", err);
+        this.setState({
+          usernameInput: "",
+          passwordInput: "",
+          confirmInput: "",
+          emailInput: "",
+          usernameInput: "",
+          message: "Error inserting user"
+        });
+      });
+  };
+
+  render() {
+    const {
+      emailInput,
+      fullNameInput,
+      usernameInput,
+      passwordInput,
+      confirmInput,
+      message,
+      registered,
+      zipcodeInput
+    } = this.state;
+    const {
+      submitForm,
+      handleEmailChange,
+      handleFullNameChange,
+      handleUsernameChange
+    } = this;
+    if (registered) {
+      axios
+        .post("/users/login", {
+          username: usernameInput,
+          password: passwordInput
+        })
+        .then(res => {
+          this.setState({
+            message: "success"
+          });
+        })
+        .catch(err => {
+          this.setState({
+            message: "username/password not found"
+          });
+        });
+      return <Redirect to="/user" />;
+    }
+    return (
+      <div>
+          <div class="login-container">
+            <div class="login-box">
+              <form onSubmit={submitForm}>
+                <h1 className="title">GameOn</h1>
+                <h2>Sign Up To Play, Compete, and Rank-Up</h2>
+                <label>
+                  <input
+                    type="text"
+                    name="Email"
+                    placeholder="Email"
+                    value={emailInput}
+                    onChange={handleEmailChange}
+                  />
+                </label>
+                <label>
+                  <input
+                    type="text"
+                    name="Full name"
+                    placeholder="Full name"
+                    value={fullNameInput}
+                    onChange={handleFullNameChange}
+                  />
+                </label>
+                <label>
+                  <input
+                    type="text"
+                    name="username"
+                    placeholder="Username"
+                    value={usernameInput}
+                    onChange={handleUsernameChange}
+                  />
+                </label>
+                <label>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={passwordInput}
+                    onChange={this.handlePasswordChange}
+                  />
+                </label>
+                <label id="confirm">
+                  <input
+                    type="password"
+                    name="confirm-input"
+                    placeholder="Confirm Password"
+                    value={confirmInput}
+                    onChange={this.handleConfirmChange}
+                  />
+                </label>
+                <label>
+                  <input
+                    type="text"
+                    name="zip_code"
+                    placeholder="Zipcode"
+                    value={zipcodeInput}
+                    onChange={this.handleZipCodeChange}
+                  />
+                </label>
+                <input type="submit" value="Sign Up" />
+              </form>
+              <p>{message}</p>
+            </div>
+            <div class="login-box">
+              Have an account? <Link to="/login">Log in</Link>
+            </div>
+          </div>
+      </div>
+    );
+  }
+}
