3350174097303e6407bf5119da9b8a6df25a8e2d
JoyceAja <ajagbe.joyce@gmail.com>
2018-03-21 17:14:53 -0400

    adding some past information

--- backend/db/dbAPI.js
+++ backend/db/dbAPI.js
@@ -1,156 +1,133 @@
 const db = require("./index");
-const helpers = require("../auth/helpers");
+const helpers = require("../auth/helper");
 
 const getUserByUsername = (username, callback) => {
   db
-    .any("SELECT * FROM users WHERE username = ${username}", {
-      username: username
-    })
+    .any(
+      "SELECT * FROM users WHERE username = ${username}", 
+      {username: username}
+    )
     .then(data => callback(null, data[0]))
     .catch(err => {
       callback(err, false);
     });
 };
 
-const registerUser = (user, callback, req, res) => {
+const registerUser = (user, callback) => {
   const newUser = {
-    userName: user.username,
-    fullName: user.fullname,
-    passwordDigest: helpers.generatePasswordDigest(user.password),
-    zipcode: user.zipcode,
-    profilePicUrl: "/images/user.png",
-    expPoints: 50,
-    email: user.email,
-    sports: user.sports,
-    zipcode: user.zipcode
-  };
-  const sports = JSON.parse(newUser.sports);
-  console.log(sports);
-  db
-    .one(
-      "INSERT INTO users(fullname, username, email, password_digest, zip_code, profile_pic, exp_points)" +
-        "VALUES (${fullName}, ${userName}, ${email}, ${passwordDigest}, ${zipcode}, ${profilePicUrl}, ${expPoints})" +
-        "RETURNING id",
-      newUser
-    )
-    .then(user => {
-      var user_id = user.id; //Hardcoded
-      if (sports && sports.length) {
-        var SQLStatement =
-          "INSERT INTO sports_proficiency (user_id, sport_id, proficiency)";
-        sports.forEach((sport, index) => {
-          console.log(sport);
-          if (index === 0) {
-            SQLStatement =
-              SQLStatement +
-              "\n" +
-              `VALUES(${user_id}, ${sport.sport_id}, ${sport.proficiency})`;
-          } else {
-            SQLStatement =
-              SQLStatement +
-              "\n" +
-              `,(${user_id}, ${sport.sport_id}, ${sport.proficiency})`;
-          }
-        });
-        SQLStatement += ";";
+      userName: user.username,
+      fullName: user.fullname,
+      passwordDigest: helpers.generatePasswordDigest(user.password),
+      zipcode: user.zipcode,
+      profilePicUrl: '/images/user.png',
+      expPoints: 50,
+      email: user.email,
+      sports: user.sports,
+      zipcode: user.zipcode
+  }
+  const sports = JSON.parse(newUser.sports)
+  console.log(sports)
+  db.one('INSERT INTO users(fullname, username, email, password_digest, zip_code, profile_pic, exp_points)' +
+          'VALUES (${fullName}, ${userName}, ${email}, ${passwordDigest}, ${zipcode}, ${profilePicUrl}, ${expPoints})'+ 
+          'RETURNING id, username, fullname, profile_pic', newUser)
+  .then((user)=> {
+    //id assigned by the database to the newly created user
+    var user_id = user.id 
+    //Check if the user selected some sports for each sport we in a hacky not sure if good way 
+    //concatenate a SQL statement string so that we insert that data in one INSERT statement at once
+    //if the user didn't picked any sport then skip this step altogether 
+    if(sports.length) {
+      //Base (header) for the SQL statement
+      var SQLStatement = 'INSERT INTO sports_proficiency (user_id, sport_id, proficiency)'
+      sports.forEach((sport, index) => {
+        console.log(sport)
+        if(index === 0) {
+          SQLStatement  =  SQLStatement + '\n' + `VALUES(${user_id}, ${sport.sport_id}, ${sport.proficiency})` 
+        } else {
+          SQLStatement = SQLStatement + '\n' + `,(${user_id}, ${sport.sport_id}, ${sport.proficiency})`
+        }
+      })
+      SQLStatement += ';' 
 
-        db
-          .none(SQLStatement)
-          .then(() => callback(null))
-          .catch(err => callback(err));
-      }
-    })
-    .catch(err => callback(err));
-};
+      //Feeding that SQL statement into the db instance of pg-promise
+      db.none(SQLStatement)
+        .then(() => callback(null, user))
+        .catch((err) => callback(err))
+    }
+  })
+  .catch(err => callback(err))
+}
 //expecting an obj for our post
 const addPosts = (postObj, callback) => {
-  db
-    .none(
-      "INSERT INTO posts(owner_id, imageurl) VALUES (${ownerId}, ${imageUrl})",
-      postObj
-    )
-    .then(() => callback(null))
-    .catch(err => callback(err));
-};
+  db.none('INSERT INTO posts(owner_id, imageurl) VALUES (${ownerId}, ${imageUrl})', postObj)
+  .then(() => callback(null))
+  .catch(err => callback(err))
+}
+
+// const follow = ()
+
 const getPosts = (username, callback) => {
   db
     .any(
       "SELECT * FROM posts WHERE owner_id = (SELECT id FROM users WHERE username = ${username})",
-      { username: username }
+      {username:username}
     )
     .then(data => callback(null, data))
     .catch(err => callback(err, false));
 };
 
-const addLike = (likesObj, callback) => {
+const postLikes = (likesObj, callback) => {
   db
-    .none(
-      "INSERT INTO likesTable(post_id, liked_by) VALUES (${postId}, ${likedBy})",
-      likesObj
-    )
-    .then(() => callback(null))
-    .catch(err => callback(err));
+  .none(
+    "INSERT INTO likesTable(post_id, liked_by) VALUES (${postId}, ${likedBy})", likesObj)
+  .then(() => callback(null))
+  .catch(err => callback(err))
 };
 
-const getLikes = (userId, callback) => {
-  db
-    .any(
-      "SELECT * FROM likesTable WHERE post_id = ANY(SELECT id FROM posts WHERE owner_id = ANY(SELECT owner_id FROM followinfo WHERE follower_id = ${userId}))",
-      { userId: userId }
-    )
-    .then(data => callback(null, data))
-    .catch(err => callback(err, false));
-};
+const getLikes = (postId, callback) => {
+  db.any('SELECT liked_by FROM likesTable WHERE post_id =${postId}', {postId:postId})
+  .then(data => callback(null, data))
+  .catch(err => callback(err, false))
+}
 
-const getFeed = (userId, callback) => {
+
+const getFeed = (username, callback) => {
   db
     .any(
-      "SELECT * FROM posts WHERE owner_id = ANY(SELECT owner_id FROM followinfo INNER JOIN users ON followinfo.owner_id = users.id WHERE follower_id = ${userId})",
-      { userId: userId }
+      "SELECT * FROM posts WHERE owner_id = ANY(SELECT follower_id FROM followinfo INNER JOIN users ON followinfo.owner_id = users.id WHERE users.username = ${username})",
+      {username:username}
     )
     .then(data => callback(null, data))
     .catch(err => callback(err, false));
 };
 
 const addFollower = (ownerId, followerId, callback) => {
-  db
-    .none(
-      "INSERT INTO followinfo (owner_id, follower_id) VALUES(${ownerId}, ${followerId})",
-      { ownerId, followerId }
-    )
+  db.none('INSERT INTO followinfo (owner_id, follower_id) VALUES(${ownerId}, ${followerId})', {ownerId, followerId})
     .then(() => callback(null))
-    .catch(err => callback(err, false));
-};
+    .catch(err => callback(err, false))
+}
 
-const getFollows = callback => {
-  db
-    .any("SELECT owner_id, follower_id from followInfo")
-    .then(data => callback(null, data))
-    .catch(err => callback(err, false));
-};
-// const follow = ()
-const getAllUsers = callback => {
-  db
-    .any("SELECT username, profile_pic, exp_points from users")
-    .then(data => callback(null, data))
-    .catch(err => callback(err, false));
-};
-
-const updateUserInfo = (userInfo, callback) => {
-  db
-    .one(
-      "UPDATE users SET username=${username},fullname=${fullname}, email=${email},zipcode=${zipcode}" +
-        ", profile_pic=${profilePic} WHERE id=${id}" +
-        " RETURNING id",
-      userInfo
-    )
-    .then(
-      db
-        .none(SQLStatement)
-        .then(() => callback(null))
-        .catch(err => callback(err))
-    );
-};
+/**
+ * Retrieves user information relevant to their profile
+ * @param {string} userId - The user id to grab the info from
+ * @param {Function} answer - Function that will be called with (err, data) as its arguments
+ */
+const getUserInfo = (userId, answer) => {
+   db.one(`SELECT id, username, fullname, email, zip_code, profile_pic, exp_points FROM users 
+           WHERE id = $1`, userId) 
+      .then(user => {
+        db.any(`SELECT sports.name, sports.id, proficiency from sports_proficiency 
+                INNER JOIN sports ON sports.id = sport_id
+                WHERE user_id = $1`, userId)
+          .then(sports => {
+            //Aggregating info from both queries into one object
+            var userInfo = {...user, sports:sports}
+            return answer(null, userInfo)
+          })
+          .catch(err => answer(err, null))
+   })
+    .catch(err =>  answer(err, null))
+}
 
 const addSport = (sport, callback) => {
   console.log("sport", sport);
@@ -178,15 +155,15 @@ const deleteSport = (sport, callback) => {
 module.exports = {
   getUserByUsername: getUserByUsername,
   registerUser: registerUser,
-  getAllUsers: getAllUsers,
-  updateUserInfo: updateUserInfo,
-  addSport: addSport,
-  deleteSport: deleteSport,
-  addPost: addPost,
+  getUserInfo: getUserInfo,
+  addPosts: addPosts,
   getPosts: getPosts,
-  addLike: addLike,
+  postLikes: postLikes,
   getLikes: getLikes,
   getFeed: getFeed,
   addFollower: addFollower,
-  getFollows: getFollows
+  getAllUsers: getAllUsers,
+  updateUserInfo: updateUserInfo,
+  addSport: addSport,
+  deleteSport: deleteSport
 };
