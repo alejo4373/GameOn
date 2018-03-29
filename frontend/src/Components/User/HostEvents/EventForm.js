ab82c358b1705163ef29fe651b55d55fe89eb3a1
JoyceAja <ajagbe.joyce@gmail.com>
2018-03-29 13:42:11 -0400

    changed the function handling the dropdown for the sports to make it more simple and clear. Also changed a few variable names

--- frontend/src/Components/User/HostEvents/EventForm.js
+++ frontend/src/Components/User/HostEvents/EventForm.js
@@ -1,8 +1,8 @@
 import React from "react";
 import axios from "axios";
-import moment from 'moment';
-import "./time.css"
-import InputMoment from 'input-moment';
+import moment from "moment";
+import "./time.css";
+import InputMoment from "input-moment";
 
 import Events from "./CreatedEvent";
 
@@ -14,9 +14,9 @@ export default class Event extends React.Component {
       Name: "",
       m: moment(),
       imgScr: null,
-      sportIDs: '',
+      sportIDs: "",
       Address: "",
-      start:false,
+      start: false,
       end: false,
       DateInfo: null,
       startTime: null,
@@ -27,12 +27,12 @@ export default class Event extends React.Component {
       long: null,
       submit: false,
       event_id: null,
-      players: '',
+      players: "",
       sports: [],
-      gameCombo: [],
-      format_id: '',
-      sport_id:'',
-      event:''
+      gameFormat: [],
+      format_id: "",
+      sport_id: "",
+      event: ""
     };
   }
 
@@ -43,93 +43,101 @@ export default class Event extends React.Component {
   };
 
   handleMoment = m => {
-    this.setState({ m })
-  }
-
-
-  //Will have to change this to receive the sport name not sport_id
-  //Get request to get possible combinations possible
-  handleSelect = e => {
-    const id = e.target.value
-    axios.get(`/sport/formats/${id}`)
-    .then(res => {
-      // const gameFormats = res.data.formats.map(elem => elem.description)
-      this.setState({
-      gameCombo: res.data.formats,
-      sport_id: Number(id)
-    })})
+    this.setState({ m });
   };
 
-  handleFormat = (e) => {
+  handleSportSelect = e => {
+    const id = e.target.value;
+    axios.get(`/sport/formats/${id}`).then(res => {
+      this.setState({
+        gameFormat: res.data.formats,
+        sport_id: Number(id)
+      });
+    });
+  };
+
+  handleSportFormat = e => {
     this.setState({
       format_id: e.target.value
-    })
-  }
+    });
+  };
 
   handleStartTime = () => {
     this.setState({
-      startTime: this.state.m.format('llll'),
-      start:false
-    })
-  }
+      startTime: this.state.m.format("llll"),
+      start: false
+    });
+  };
 
   handleEndTime = () => {
     this.setState({
-      endTime: this.state.m.format('llll'),
+      endTime: this.state.m.format("llll"),
       end: false
-    })
-  }
+    });
+  };
 
-  handleToggle = (e) => {
-    let name = e.target.name
+  handleToggle = e => {
+    let name = e.target.name;
     this.setState({
       [e.target.name]: !this.state.name
-    })
-  }
+    });
+  };
 
-  loadPage = (id) => {
-    axios.get(`/event/info/${id}`)
-    .then(res =>
-      this.setState({
-        event: res.data,
-        submit: true
+  loadPage = id => {
+    axios
+      .get(`/event/info/${id}`)
+      .then(res =>
+        this.setState({
+          event: res.data,
+          submit: true
+        })
+      )
+      .catch(err => console.log(err));
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const {
+      Name,
+      Address,
+      DateInfo,
+      startTime,
+      imgScr,
+      endTime,
+      Description,
+      sport_id,
+      lat,
+      long,
+      format_id
+    } = this.state;
+
+    axios
+      .post("/event/add", {
+        name: Name,
+        lat: 40.755603,
+        long: -73.984931,
+        location: Address,
+        start_ts: new Date(startTime).getTime(),
+        end_ts: new Date(endTime).getTime(),
+        description: Description,
+        sport_id: sport_id,
+        sport_format_id: format_id,
+        event_pic: imgScr
       })
-    )
-    .catch(err => console.log(err))
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault()
-      const {Name, Address, DateInfo, startTime,imgScr, endTime, Description, sport_id, lat, long, format_id} = this.state
-
-      axios.post('/event/add',
-      {name:Name,
-      lat:40.755603,
-      long:-73.984931,
-      location:Address,
-      start_ts: new Date(startTime).getTime(),
-      end_ts: new Date(endTime).getTime(),
-      description:Description,
-      sport_id:sport_id,
-      sport_format_id: format_id,
-      event_pic:imgScr
-   })
-   .then(res => 
-     this.loadPage(res.data.event.id))
-  .catch(err => console.log(err))
-  }
+      .then(res => this.loadPage(res.data.event.id))
+      .catch(err => console.log(err));
+  };
 
   componentDidMount() {
-    axios.get("/user/sports").then(res => {
-      console.log(res.data)
-      const names = res.data.sports.map(sport => sport.name)
-      const id = res.data.sports.map(sport => sport.id)
-      this.setState({
-        sports: names,
-        sportIDs: id
-      });
-    })
-    .catch(err => console.log(err));
+    axios
+      .get("/user/sports")
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          sports: res.data.sports
+        });
+      })
+      .catch(err => console.log(err));
   }
 
   form = () => {
@@ -145,7 +153,7 @@ export default class Event extends React.Component {
       start,
       end,
       team,
-      gameCombo
+      gameFormat
     } = this.state;
 
     return (
@@ -153,12 +161,14 @@ export default class Event extends React.Component {
         <h1>Create An Event</h1>
 
         <form onSubmit={this.handleSubmit}>
-        {<input
-          type='text'
-          name= 'text'
-          value= {imgScr}
-          onChange={this.handleChange}
-          />}
+          {
+            <input
+              type="text"
+              name="text"
+              value={imgScr}
+              onChange={this.handleChange}
+            />
+          }
 
           <input
             type="text"
@@ -176,46 +186,57 @@ export default class Event extends React.Component {
             onInput={this.handleChange}
           />
 
-          <button name= 'start' onClick= {this.handleToggle}>Start Time</button>
+          <button name="start" onClick={this.handleToggle}>
+            Start Time
+          </button>
           <div className="input">
             <input type="text" value={this.state.startTime} readOnly />
           </div>
-          {start? 
-          <InputMoment
-            name = 'startTime'
-            moment={this.state.m}
-            onChange={this.handleMoment}
-            minStep={5}
-            onSave={this.handleStartTime}
-          />
-          : ''
-          }
+          {start ? (
+            <InputMoment
+              name="startTime"
+              moment={this.state.m}
+              onChange={this.handleMoment}
+              minStep={5}
+              onSave={this.handleStartTime}
+            />
+          ) : (
+            ""
+          )}
 
-          <button name = 'end' onClick= {this.handleToggle}>End Time</button>
-         
+          <button name="end" onClick={this.handleToggle}>
+            End Time
+          </button>
+
           <div className="input">
             <input type="text" value={this.state.endTime} readOnly />
           </div>
 
-          {end? 
-          <InputMoment
-            name = 'endTime'
-            moment={this.state.m}
-            onChange={this.handleMoment}
-            minStep={5}
-            onSave={this.handleEndTime}
-          /> : ''
-          }
+          {end ? (
+            <InputMoment
+              name="endTime"
+              moment={this.state.m}
+              onChange={this.handleMoment}
+              minStep={5}
+              onSave={this.handleEndTime}
+            />
+          ) : (
+            ""
+          )}
 
-          <select onChange={this.handleSelect}>
+          <select onChange={this.handleSportSelect}>
             {["", ...sports].map((sport, idx) => (
-              <option key={idx} value={sport? this.state.sportIDs[idx-1] : ''}>
-                {sport}
+              <option key={idx} value={sport.id}>
+                {sport.name}
               </option>
+            ))}wq
+          </select>
+
+          <select class="team" onChange={this.handleSportFormat}>
+            {["", ...gameFormat].map(game => (
+              <option value={game.id}>{game.description}</option>
             ))}
           </select>
-          
-          <select class="team" onChange= {this.handleFormat}>{['', ...gameCombo].map(game=> <option value={ game.id }>{ game.description }</option>)}</select>
 
           <input
             type="textarea"
@@ -231,8 +252,7 @@ export default class Event extends React.Component {
     );
   };
   render() {
-    const { submit, sports, event } = this.state;
+    const { submit, event } = this.state;
     return <div>{submit ? <Events event={event} /> : this.form()}</div>;
   }
 }
-
