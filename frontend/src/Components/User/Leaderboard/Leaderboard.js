bf0dc8d6e52a2bf77daabde93951952a643e76c6
romie151 <romiezelaya@ac.c4q.nyc>
2018-04-05 13:02:51 -0400

    leaderboard colors

--- frontend/src/Components/User/Leaderboard/Leaderboard.js
+++ frontend/src/Components/User/Leaderboard/Leaderboard.js
@@ -34,90 +34,104 @@ class Leaderboard extends Component {
       <div id="leaderboard-body">
         <div id="leaderboard-container">
           <h2 id="leaderboard_header">LEADERBOARD</h2>
-          <div id="player-container">
-            <div id="top3">
-              {topThree.map((p, i) => {
-                if (i === 0) {
-                  return (
-                    <div id={`player-${i}`}>
-                      <img
-                        id={`player-${i}-pic`}
-                        src={p.profile_pic}
-                        width={"125px"}
-                        alt=""
-                      />
-                      <h5 id="top3-username" style={{ color: "black" }}>{p.username}</h5>
-                      <h5 id="top3-exp"style={{ color: "black" }}>{p.exp_points}</h5>
-                    </div>
-                  );
-                } else if (i === 1) {
-                  return (
-                    <div id={`player-${i}`}>
-                      <img
-                        id={`player-${i}-pic`}
-                        src={p.profile_pic}
-                        width={"100px"}
-                        alt=""
-                      />
-                      <h5 id="top3-username" style={{ color: "black" }}>{p.username}</h5>
-                      <h5 id="top3-exp"style={{ color: "black" }}>{p.exp_points}</h5>
-                    </div>
-                  );
-                }  else if (i === 2 ) {
-                  return (
-                    <div id={`player-${i}`}>
-                      <img
-                        id={`player-${i}-pic`}
-                        src={p.profile_pic}
-                        width={"75px"}
-                        alt=""
-                      />
-                      <h5 id="top3-username" style={{ color: "black" }}>{p.username}</h5>
-                      <h5 id="top3-exp"style={{ color: "black" }}>{p.exp_points}</h5>
-                    </div>
-                  );
-                }
-                <div id={`player-${i}`}>
-                  <h5 style={{ color: "black" }}>{p.username}</h5>
-                  <img
-                    id={`player-${i}-pic`}
-                    src={p.profile_pic}
-                    width={"75px"}
-                    alt=""
-                  />
-                  <h5 style={{ color: "black" }}>{p.exp_points}</h5>
-                </div>;
-              })}
-            </div>
-          </div>
-          <div className="headers" />
-          <div id="leaderboard-table-container">
-            <div className="headers-container">
-              <h3 id="rank">Rank</h3>
-              <h3 id="player">Player</h3>
-              <h3 id="score">Score</h3>
-            </div>
-            {players.map((p, i) => {
-              if (i > 2) {
-                return (
-                  <div className="player-card">
-                    <p id="rank-number">{i + 1}</p>
-                    <div id="player-info">
-                      <span>
+          <div id="leaderboard-grid">
+            <div id="player-container">
+              <div id="top3">
+                {topThree.map((p, i) => {
+                  if (i === 0) {
+                    return (
+                      <div id={`player-${i}`}>
                         <img
                           id={`player-${i}-pic`}
                           src={p.profile_pic}
-                          width={"50px"}
+                          width={"125px"}
                           alt=""
                         />
-                      </span>
-                      <span>{p.username}</span>
+                        <h5 id="top3-username" style={{ color: "black" }}>
+                          {p.username}
+                        </h5>
+                        <h5 id="top3-exp" style={{ color: "black" }}>
+                          {p.exp_points}
+                        </h5>
+                      </div>
+                    );
+                  } else if (i === 1) {
+                    return (
+                      <div id={`player-${i}`}>
+                        <img
+                          id={`player-${i}-pic`}
+                          src={p.profile_pic}
+                          width={"100px"}
+                          alt=""
+                        />
+                        <h5 id="top3-username" style={{ color: "black" }}>
+                          {p.username}
+                        </h5>
+                        <h5 id="top3-exp" style={{ color: "black" }}>
+                          {p.exp_points}
+                        </h5>
+                      </div>
+                    );
+                  } else if (i === 2) {
+                    return (
+                      <div id={`player-${i}`}>
+                        <img
+                          id={`player-${i}-pic`}
+                          src={p.profile_pic}
+                          width={"75px"}
+                          alt=""
+                        />
+                        <h5 id="top3-username" style={{ color: "black" }}>
+                          {p.username}
+                        </h5>
+                        <h5 id="top3-exp" style={{ color: "black" }}>
+                          {p.exp_points}
+                        </h5>
+                      </div>
+                    );
+                  }
+                  <div id={`player-${i}`}>
+                    <h5 style={{ color: "black" }}>{p.username}</h5>
+                    <img
+                      id={`player-${i}-pic`}
+                      src={p.profile_pic}
+                      width={"75px"}
+                      alt=""
+                    />
+                    <h5 style={{ color: "black" }}>{p.exp_points}</h5>
+                  </div>;
+                })}
+              </div>
+            </div>
+            <div className="headers" />
+            <div id="leaderboard-table-container">
+              <div className="headers-container">
+                <h3 id="rank">Rank</h3>
+                <h3 id="player">Player</h3>
+                <h3 id="score">Score</h3>
+              </div>
+              {players.map((p, i) => {
+                if (i > 2) {
+                  return (
+                    <div className="player-card">
+                      <p id="rank-number">{i + 1}</p>
+                      <div id="player-info">
+                        <span>
+                          <img
+                            id={`player-${i}-pic`}
+                            src={p.profile_pic}
+                            width={"50px"}
+                            alt=""
+                          />
+                        </span>
+                        <span>{p.username}</span>
+                      </div>
+                      <p id="score-points">{p.exp_points}</p>
                     </div>
-                    <p id="score-points">{p.exp_points}</p>
-                  </div>
-                );
-              }
-            })}
+                  );
+                }
+              })}
+            </div>
           </div>
         </div>
       </div>
