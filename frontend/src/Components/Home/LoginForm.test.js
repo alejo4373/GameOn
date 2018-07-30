7fbf4f14815520a6b279c1d795d0cb74e973ac76
alejo4373 <alejo4373@gmail.com>
2018-07-30 00:53:20 -0400

    LoginForm tests ideation

--- frontend/src/Components/Home/LoginForm.test.js
+++ frontend/src/Components/Home/LoginForm.test.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 import LoginForm from './LoginForm';
 
-it('click to submit form', (done) => {
-  const callback = (err, user) => {
-    expect(err).toEqual(new Error("Network Error"))
-    done();
-  }
+describe('<LoginForm/>', () => {
+  it('form renders as it did last time (snapshot)', () => {
+  const component = renderer.create(<LoginForm/>)
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  })
 
-  const wrapper = mount(
-    <LoginForm handleLoginResponse={callback}/> 
-  );
-  wrapper.find('form').simulate('submit');
+  it('click to submit form', (done) => {
+    const callback = (err, user) => {
+      expect(err).toEqual(new Error("Network Error"))
+      done();
+    }
+
+    const wrapper = mount(
+      <LoginForm handleLoginResponse={callback}/> 
+    );
+    wrapper.find('form').simulate('submit');
+  });
+
+  it('has two input texts, one of them is type password');
+  it('doesn\'t submit if either input field is empty' );
+  it('has a submit button with a function attached to it');
 });
