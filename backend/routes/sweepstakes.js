dcb1cbcf865e8f1372a8bd36a92ab76eac777b2f
alejandro <alejo4373@gmail.com>
2019-05-05 20:25:22 -0400

    insert to sweepstakes data to db

--- backend/routes/sweepstakes.js
+++ backend/routes/sweepstakes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../db');
 
-router.post('/submit', (req, res, next) => {
+router.post('/submit', async (req, res, next) => {
   try {
+    await db.any('INSERT INTO sweepstakes(time_stamp, email, body) VALUES(${time_stamp}, ${email}, ${body})', {
+      time_stamp: new Date().toISOString(),
+      email: req.body.Email,
+      body: JSON.stringify(req.body)
+    })
     res.status(200).json(req.body)
   } catch (err) {
     console.error(err);
